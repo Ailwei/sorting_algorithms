9458e71656d24c9c5659757ba685ba0a0d01a425
Ailwei <Ailweimaemu@gmail.com>
2023-11-17 03:38:49 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -26,7 +26,10 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
+void quick_sort(int *array, size_t size);
+void swap(int *a, int *b);
+int lomuto_partition(int *array, int low, int high);
+void quick_sort_helper(int *array, int low, int high);
 
 
 
