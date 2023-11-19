f1588634585042a2b8014ab31225a232e2e17aa1
Ailwei <Ailweimaemu@gmail.com>
2023-11-19 13:47:43 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -31,6 +31,8 @@ void swap(int *a, int *b);
 int lomuto_partition(int *array, int low, int high, size_t size);
 void quick_sort_helper(int *array, int low, int high);
 void shell_sort(int *array, size_t size);
+void change_nodes(listint_t **list, listint_t *node1, listint_t *node2);
+void cocktail_sort_list(listint_t **list);
 
 
 
