a3a3efd83e74540419d100cd85ec35a7dc322dc5
Ailwei <Ailweimaemu@gmail.com>
2023-11-20 02:13:07 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - doubly linked list node
@@ -33,6 +34,8 @@ void quick_sort_helper(int *array, int low, int high);
 void shell_sort(int *array, size_t size);
 void change_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+int get_max(int *array, int size);
 
 
 
