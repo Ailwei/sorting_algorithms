9f3c3210bcc9def941a0295cc2428a032c7930b4
Ailwei <Ailweimaemu@gmail.com>
2023-11-20 07:52:03 -0800

    8. Heap sort

---
+++ 104-heap_sort.c
@@ -0,0 +1,81 @@
+#include "sort.h"
+
+/**
+ * sift_down - Perform the sift-down operation in the heap sort algorithm.
+ * @array: The array to sort.
+ * @size: The size of the array.
+ * @root: The root index of the heap.
+ * @end: The end index of the heap.
+ */
+void sift_down(int *array, size_t size, size_t root, size_t end)
+{
+	size_t max_child, child, swap;
+
+	while ((root * 2 + 1) <= end)
+	{
+		max_child = root * 2 + 1;
+		child = max_child + 1;
+
+		if (child <= end && array[child] > array[max_child])
+			max_child = child;
+
+		if (array[root] < array[max_child])
+		{
+			swap = array[root];
+			array[root] = array[max_child];
+			array[max_child] = swap;
+
+			print_array(array, size);
+			root = max_child;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+/**
+ * heapify - Build a max heap from the array.
+ * @array: The array to sort.
+ * @size: The size of the array.
+ */
+void heapify(int *array, size_t size)
+{
+	ssize_t start = (size - 2) / 2;
+
+	while (start >= 0)
+	{
+		sift_down(array, size, start, size - 1);
+		start--;
+	}
+}
+
+/**
+ * heap_sort - Sort an array of integers in ascending order using Heap sort.
+ * @array: The array to sort.
+ * @size: The size of the array.
+ */
+void heap_sort(int *array, size_t size)
+{
+	size_t end;
+
+	if (array == NULL || size < 2)
+		return;
+
+	heapify(array, size);
+
+	end = size - 1;
+	while (end > 0)
+	{
+		int swap = array[end];
+		array[end] = array[0];
+		array[0] = swap;
+
+		print_array(array, size);
+		end--;
+
+		sift_down(array, size, 0, end);
+	}
+}
+
